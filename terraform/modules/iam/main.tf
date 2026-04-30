3840d88e927c971e7a5ee5b230efd836df8e5535
gpelipas <7575993+gpelipas@users.noreply.github.com>
2026-04-30 17:02:28 -0400

    added eks alb role

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -116,3 +116,42 @@ resource "aws_iam_role_policy_attachment" "github_eks" {
 
 output "app_role_arn"            { value = aws_iam_role.app.arn }
 output "github_actions_role_arn" { value = aws_iam_role.github_actions.arn }
+
+# AWS Load Balancer Controller IAM policy
+data "http" "alb_controller_policy" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json"
+}
+
+resource "aws_iam_policy" "alb_controller" {
+  name   = "${local.name}-alb-controller-policy"
+  policy = data.http.alb_controller_policy.response_body
+}
+
+# AWS Load Balancer Controller IRSA role
+resource "aws_iam_role" "alb_controller" {
+  name = "${local.name}-alb-controller-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = var.oidc_provider_arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${local.oidc_sub}" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
+          "${local.oidc_aud}" = "sts.amazonaws.com"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "alb_controller" {
+  role       = aws_iam_role.alb_controller.name
+  policy_arn = aws_iam_policy.alb_controller.arn
+}
+
+output "alb_controller_role_arn" { value = aws_iam_role.alb_controller.arn }
