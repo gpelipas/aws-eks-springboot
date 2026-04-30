95befee75651a4ac63dabeffe6334826833b556e
gpelipas <7575993+gpelipas@users.noreply.github.com>
2026-04-30 17:06:00 -0400

    added eks alb role

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,3 +74,60 @@ module "iam" {
   oidc_issuer_url  = module.eks.oidc_issuer_url
   oidc_provider_arn = module.eks.oidc_provider_arn
 }
+
+# --- Helm provider for installing Kubernetes apps via Terraform ---
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_ca)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+      command     = "aws"
+    }
+  }
+}
+
+# --- AWS Load Balancer Controller ---
+resource "helm_release" "alb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.11.0"
+
+  depends_on = [
+    module.eks,
+    module.iam
+  ]
+
+  set {
+    name  = "clusterName"
+    value = module.eks.cluster_name
+  }
+
+  set {
+    name  = "serviceAccount.create"
+    value = "true"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = module.iam.alb_controller_role_arn
+  }
+
+  set {
+    name  = "region"
+    value = var.aws_region
+  }
+
+  set {
+    name  = "vpcId"
+    value = module.vpc.vpc_id
+  }
+}
--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -132,3 +132,4 @@ output "cluster_endpoint"        { value = aws_eks_cluster.main.endpoint }
 output "node_security_group_id"  { value = aws_security_group.nodes.id }
 output "oidc_issuer_url"         { value = aws_eks_cluster.main.identity[0].oidc[0].issuer }
 output "oidc_provider_arn"       { value = aws_iam_openid_connect_provider.cluster.arn }
+output "cluster_ca" { value = aws_eks_cluster.main.certificate_authority[0].data }
